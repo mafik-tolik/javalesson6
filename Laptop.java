b6cf568543e741a2969b017b282b7bb0e72a7e95
mafik_tolik <mafik_tolik@mail.ru>
2023-02-18 13:08:44 +0300

    Commit 1

---
+++ Laptop.java
@@ -0,0 +1,14 @@
+public class Laptop {
+
+    int id;
+    String name;
+    String ram;
+    String hdd;
+    String os;
+    String colour;
+    String price;
+
+    public String toString() {
+        return "id: " + id + ", " + name + ", ОЗУ: " + ram + ", HDD/SSD: " + hdd + ", ОС: " + os + ", цвет: " + colour + ", цена: " + price + " руб.";
+    }
+}
